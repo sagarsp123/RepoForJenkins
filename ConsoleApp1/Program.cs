e67118881e084bef87b61e149ae512336feb1f38
Sagar Prabhu <48390647+sagarsp123@users.noreply.github.com>
2020-11-18 15:44:39 +0530

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Trying Trigger of Jenkins");
             Console.Read();
         }
     }
